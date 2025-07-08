11abde777bc9d14bcd5a84218f7798881b25a83d
Indominus-labs-org <62879169+vaisx05@users.noreply.github.com>
2025-07-08 15:47:05 +0530

    Update BASE_URL to production endpoint
    Changed BASE_URL from local development server to the production URL. The local URL is now commented out for reference.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://voiceiq-db.indominuslabs.in";
-export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "https://vq-1.indominuslabs.in";
+//export const BASE_URL = "http://127.0.0.1:8000";
